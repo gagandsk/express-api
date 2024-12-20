cac037ddf49589b1c7057c7fb3c8725fc21fc394
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2024-12-20 11:22:31 +0100

    Primer servidor HTTP con express.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('My App');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola, mi primer server con express');
+})
+
+
+app.listen(port, () => {
+  console.log('Servidor corriendo en el puerto ' + port);
+});
