7c5215957e93f9b09e40620dcedbccbc7e942874
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2024-12-20 11:42:50 +0100

    Routing con Express.js

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hola, mi primer server con express');
 })
 
+app.get('/example', (req, res) =>{
+  res.send('example')
+})
+
+//json = JavaScript Object Notation
+app.get('/products', (req, res) =>{
+  res.json({
+    name: 'product 1',
+    price: 200
+  })
+})
 
 app.listen(port, () => {
   console.log('Servidor corriendo en el puerto ' + port);
