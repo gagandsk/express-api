20977191f674d0092c9777999c0edf333c2ad1bd
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2024-12-20 12:57:39 +0100

    Routing con Express.js, nuevas rutas

--- index.js
+++ index.js
@@ -16,7 +16,43 @@ app.get('/products', (req, res) =>{
     name: 'product 1',
     price: 200
   })
-})
+});
+
+app.get('/categories', (req, res) => {
+  res.json({
+    categories: [
+      'Sedan',
+      'SUV',
+      'Hatchback',
+      'Convertible',
+      'Coupe',
+      'Wagon',
+      'Pickup Truck',
+      'Van',
+      'Minivan',
+      'Sports Car',
+      'Luxury Car',
+      'Electric Car',
+      'Hybrid Car',
+      'Crossover',
+      'Off-Road'
+    ],
+
+    luxuryCars: {
+      BMW: 'Luxury Car',
+      Porsche: 'Sports Car',
+      'Aston Martin': 'Sports Car',
+      Bentley: 'Luxury Car',
+      RollsRoyce: 'Luxury Car',
+      Ferrari: 'Sports Car',
+      Lamborghini: 'Sports Car',
+      Maserati: 'Luxury Car',
+      Bugatti: 'Hypercar',
+      McLaren: 'Supercar',
+      Tesla: 'Electric Car'
+    },
+  })
+});
 
 app.listen(port, () => {
   console.log('Servidor corriendo en el puerto ' + port);
