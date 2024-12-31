daf6ec3d02c3e5bbeadbd1d7978247f3246846a6
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2024-12-31 17:34:50 +0100

    CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/errorHandler');
@@ -7,6 +8,19 @@ const app = express();
 const port = 3000;
 
 app.use(express.json()); //middleware que nos permite recibir datos por el metodo 'post'
+
+const whiteList = ['http://localhost:8080', 'http://127.0.0.1:5500' , 'http://myapp.com'];
+const options = {
+  origin: (origin, callback) => {
+    if(whiteList.includes(origin)){
+      callback(null, true)
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola, mi primer server con express');
